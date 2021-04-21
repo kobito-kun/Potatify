c3be0a61fe9d0e958fe41b16639af55a181f981e
ItoKobi <ito.kobilol@gmail.com>
2021-04-20 22:15:15 -0400

    Product Page

--- src/urls.py
+++ src/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from ecommerce.views import *
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', mainPage, name="main"),
-]
+    path('<str:title>', individualPage, name="product"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
